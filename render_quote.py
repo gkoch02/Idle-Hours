8923578dbd1cc7e6b3eb36b8b7c827a5f1c62e4c
Greg Koch <gkoch02@users.noreply.github.com>
2026-04-17 16:23:05 -0700

    Revert Spectra yellow background

--- render_quote.py
+++ render_quote.py
@@ -22,7 +22,7 @@ SPECTRA6 = {
     "green": (0, 255, 0),
 }
 SPECTRA6_PALETTE = list(SPECTRA6.values())
-PAGE_BG = SPECTRA6["yellow"]
+PAGE_BG = SPECTRA6["white"]
 TEXT = SPECTRA6["black"]
 SUBTLE = SPECTRA6["black"]
 FAINT = SPECTRA6["black"]
